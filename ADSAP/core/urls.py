46bfa782ab00eb473b7fc00aed6172f2054cc8c6
Gabriel Davila <l20211769@tectijuana.edu.mx>
2024-05-04 19:49:26 -0700

    Static media 1.1

--- ADSAP/core/urls.py
+++ ADSAP/core/urls.py
@@ -17,5 +17,11 @@ urlpatterns = [
     path('vacaciones/solicitud/confirmacion', views.Vacaciones_Confirmacion.as_view(), name="vacaciones_confirmacion"),
     path('vacaciones/solicitud/estado/<int:pk>/', views.Vacaciones_Estado.as_view(), name="vacaciones_estado"),
     path('vacaciones/solicitud/eliminar/<int:pk>/', views.Vacaciones_Eliminacion.as_view(), name="vacaciones_eliminar_confirmacion"),
+    path('permisos', views.Permisos.as_view(), name="permisos"),
+    path('permisos/solicitud', views.Permisos_Form, name="permisos_solicitud"),
+    path('permisos/solicitud/confirmacion', views.Permisos_Confirmacion.as_view(), name="permisos_confirmacion"),
+    path('permisos/solicitud/estado/<int:pk>/', views.Permisos_Estado.as_view(), name="permisos_estado"),
+    path('permisos/solicitud/eliminar/<int:pk>/', views.Permisos_Eliminacion.as_view(), name="permisos_eliminar_confirmacion"),
+    path('permisos/filtro', views.searchPermisos, name="permisos_filter"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
