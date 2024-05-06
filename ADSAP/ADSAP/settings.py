89bfa45e1cec93b17db979a53ab570ce21ceb815
Gabriel Davila <l20211769@tectijuana.edu.mx>
2024-05-06 15:42:25 -0700

    DEBUG TEST 1.1

--- ADSAP/ADSAP/settings.py
+++ ADSAP/ADSAP/settings.py
@@ -33,6 +33,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
