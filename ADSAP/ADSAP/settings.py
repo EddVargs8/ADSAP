45af756c25595e28239ebe81df6d2f24b69e8edd
Gabriel Davila <l20211769@tectijuana.edu.mx>
2024-05-06 15:46:27 -0700

    DEBUG TEST 1.2

--- ADSAP/ADSAP/settings.py
+++ ADSAP/ADSAP/settings.py
@@ -38,6 +38,8 @@ if not DEBUG:    # Tell Django to copy static assets into a path called `staticf
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Application definition
 
