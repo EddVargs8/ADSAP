9abab30c9c657b4e225e6ebb1d2abd94160f3847
Gabriel Davila <l20211769@tectijuana.edu.mx>
2024-05-06 16:21:05 -0700

    DEBUG TEST 1.4

--- ADSAP/ADSAP/settings.py
+++ ADSAP/ADSAP/settings.py
@@ -29,15 +29,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
